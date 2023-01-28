9206c2a581e865e3f16fbea833bb526cacec0a39
Francesco Fantinel <francescoacf@outlook.com>
2023-01-28 10:13:17 -0300

    feat: create class ProductInOut

---
+++ src/MiniERP.Models/ProductInOut.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniERP.Models
+{
+    public class ProductInOut : BaseIdentifier
+    {
+        public Guid ProductId { get; set; }
+
+        public Product? Product { get; set; }
+
+        public decimal? QuantityIn { get; set; }
+
+        public decimal? QuantityOut { get; set; }
+
+        public decimal Price { get; set; }
+
+        public decimal UnitPrice { get; set; }
+
+        public decimal AveragePrice { get; set; }
+    }
+}
+
