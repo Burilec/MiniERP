fd46f9821cd9ed90528d4b0d07e52151562c7a49
Cristian Burile <cristianburile@hotmail.com>
2023-01-28 15:58:39 -0300

    feat: Add a dialog window

---
+++ src/MiniERP.UI/Views/Dialogs/Windows/DialogWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Prism.Services.Dialogs;
+
+namespace MiniERP.UI.Views.Dialogs.Windows
+{
+    internal sealed partial class DialogWindow : IDialogWindow
+    {
+        internal DialogWindow()
+        {
+            InitializeComponent();
+        }
+
+        public IDialogResult Result { get; set; } = null!;
+    }
+}
