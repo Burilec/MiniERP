c20805097a1d79d886ba781a312cea94f165ad3e
Francesco Fantinel <francescoacf@outlook.com>
2023-01-28 10:14:00 -0300

    feat: create class ProductSummary

---
+++ src/MiniERP.Models/ProductSummary.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniERP.Models
+{
+    public class ProductSummary : BaseIdentifier
+    {
+        public Guid ProductId { get; set; }
+
+        public Product? Product { get; set; }
+
+        public decimal AveragePrice { get; set; }
+
+        public decimal LastPriceSell { get; set; }
+
+        public decimal LastPriceBuy { get; set; }
+
+        public decimal Quantity { get; set; }
+    }
+}
