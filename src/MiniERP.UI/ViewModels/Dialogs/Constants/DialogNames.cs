2bfb6707e648494c3105761d2314c2ef8a7069d1
Francesco Fantinel <francescoacf@outlook.com>
2023-02-03 09:48:47 -0300

    feat: add Unit Dialog

--- src/MiniERP.UI/ViewModels/Dialogs/Constants/DialogNames.cs
+++ src/MiniERP.UI/ViewModels/Dialogs/Constants/DialogNames.cs
@@ -3,5 +3,8 @@
     internal static class DialogNames
     {
         internal const string AddOrEditProductDialogName = "AddOrEditProductDialog";
+        internal const string AddOrEditUnitDialogName = "AddOrEditUnitDialog";
     }
+
+    
 }
