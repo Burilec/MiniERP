381441aeeb488b0579adce9c5a60433db553f3c5
Francesco Fantinel <francescoacf@outlook.com>
2023-02-11 15:17:22 -0300

    feat: add some properties

--- src/MiniERP.Models/Unit.cs
+++ src/MiniERP.Models/Unit.cs
@@ -1,8 +1,35 @@
-﻿namespace MiniERP.Models
+﻿using MiniERP.Models.Exceptions;
+
+namespace MiniERP.Models
 {
     public sealed class Unit : BaseIdentifier
     {
         public string Name { get; set; }
         public string Description { get; set; }
+
+        public Unit(string name, string description) {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentException(Messages.ValueCannotBeNullOrWhitespace, nameof(name));
+
+            if (string.IsNullOrWhiteSpace(description))
+                throw new ArgumentException(Messages.ValueCannotBeNullOrWhitespace, nameof(description));
+
+            Name = name;
+            Description = description;
+        }
+
+        public void SetName(string name) {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentException(Messages.ValueCannotBeNullOrWhitespace, nameof(name));
+
+            Name = name;
+        }
+
+        public void SetDescription(string description) {
+            if (string.IsNullOrWhiteSpace(description))
+                throw new ArgumentException(Messages.ValueCannotBeNullOrWhitespace, nameof(description));
+
+            Description = description;
+        }
     }
 }
